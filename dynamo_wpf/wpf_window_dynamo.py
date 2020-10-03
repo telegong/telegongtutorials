cca2607f84ef6e9a524047ce92a78c1fe07a6c3d
Eugene Choi <tnx2u2@gmail.com>
2020-10-04 04:50:17 +0900

    RPS dynamo 구분용 __name__ == '__main__' 확인

--- dynamo_wpf/wpf_window_dynamo.py
+++ dynamo_wpf/wpf_window_dynamo.py
@@ -1,24 +1,34 @@
 # Python 표준 및 DesignScript 라이브러리 로드
 import sys
-import System
 import clr
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
-clr.AddReference("PresentationFramework")
-from System.Windows import * 
+import System.Windows
+from System.Threading import Thread, ThreadStart, ApartmentState
+clr.AddReference('PresentationFramework')
+from System.Windows import *
 
 # 이 노드에 대한 입력값은 IN 변수에 리스트로 저장됩니다.
-dataEnteringNode = IN[0]
+if __name__== '__main__': IN = True
+dataEnteringNode = IN
 
 # 코드를 이 선 아래에 배치
-window = Window()
-window.Title = "Hello WPF!"
-button = Controls.Button()
-button.Content = "Push Me!  "+ str(dataEnteringNode)
-panel = Controls.StackPanel()
-window.Content = panel
-panel.Children.Add(button)
-window.Show()
-	
+
+# Initialization Constants
+Window = System.Windows.Window
+Application = System.Windows.Application
+def appThread():
+	window = Window()
+	window.Title = "Hello WPF!"
+	button = Controls.Button()
+	button.Content = "Push Me!  "+ str(dataEnteringNode)
+	panel = Controls.StackPanel()
+	window.Content = panel
+	panel.Children.Add(button)
+	window.Show()
+if System.Diagnostics.Process.GetCurrentProcess().ProcessName == "Revit":
+	thread = appThread()
+else:
+	thread = Application.Current.Dispatcher.Invoke(appThread)
 # 출력을 OUT 변수에 지정합니다.
 OUT = 0
